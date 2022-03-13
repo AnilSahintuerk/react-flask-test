e89a53d260ba052748998ea0eb8b21f4b9c8910a
AnilSahintuerk <sahintuerk.anil@gmail.com>
2022-03-13 10:53:47 +0100

    added functionality to add new artists and delete existing ones

--- flask-server/server.py
+++ flask-server/server.py
@@ -6,9 +6,36 @@ from database import Artist
 
 # ARTISTS = [{'name': 'Salvador Dali'}, {'name':'Leonardo DaVinci'}, {'name':'Vincent Van Gogh'}]
 
+parser = reqparse.RequestParser()
+parser.add_argument('name')
+parser.add_argument('id')
+
+
+
 class Artists(Resource):
     def get(self):
         return jsonify(Artist.query.all())
+    
+    def post(self, age=30):
+        args = parser.parse_args()
+        aName = args['name']
+        
+        if aName == '':
+            return
+        
+        artist = Artist(name=aName, age=age)
+        db.session.add(artist)
+        db.session.commit()
+        return
+    
+    def delete(self):
+        args = parser.parse_args()
+        artistId = args['id']
+        delArtist = Artist.query.filter_by(id=artistId).first()
+        print(delArtist)
+        db.session.delete(delArtist)
+        db.session.commit()
+        return
     
     
 api.add_resource(Artists, '/artists')
