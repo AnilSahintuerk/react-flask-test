887fd2a737717ea72cb37d06509b0020d05d88f6
AnilSahintuerk <sahintuerk.anil@gmail.com>
2022-03-13 10:54:26 +0100

    added api fetch call to add and delete artists

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,21 +4,54 @@ import { useState, useEffect } from "react";
 function App() {
   const [data, setData] = useState([{}]);
 
+  const addArtistHandler = async (event) => {
+    // event.preventDefault();
+    const name = event.target.name.value;
+    fetch("/artists", {
+      method: "POST",
+      body: JSON.stringify({ name: name }),
+      headers: { "Content-Type": "application/json" },
+    });
+  };
+
+  const deleteArtistHandler = async (event, artistId) => {
+    // event.preventDefault();
+    console.log(artistId);
+    await fetch("/artists", {
+      method: "DELETE",
+      body: JSON.stringify({ id: artistId }),
+      headers: { "Content-Type": "application/json" },
+    });
+  };
+
   useEffect(() => {
     fetch("/artists")
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
 
-  console.log(data);
-
   return (
     <div className="App">
       <p style={{ paddingBottom: "3rem" }}>Hello World</p>
+      <form method="put" id="form1" onSubmit={addArtistHandler}>
+        <label>Artist Name:</label>
+        <input type="text" id="name" name="name" />
+        <input type="submit" value="Submit" />
+      </form>
       {typeof data == "undefined" ? (
         <p> Loading ...</p>
       ) : (
-        data.map((data, index) => <p key={index}>{data.name}</p>)
+        data.map((data, index) => (
+          <form
+            method="delete"
+            index={index}
+            id="existingArtist"
+            onSubmit={(event) => deleteArtistHandler(event, data.id)}
+          >
+            <span key={index}>{data.name}</span>
+            <input index={index} type="submit" value="Delete" />
+          </form>
+        ))
       )}
     </div>
   );
